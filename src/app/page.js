65bd5f8fddc52f167748fee6362cda3f99ca6227
salman <salmancrc9@gmail.com>
2025-11-12 23:37:33 +0500

    Remove placeholder heading from Home component in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,6 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
