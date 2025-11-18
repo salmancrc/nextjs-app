2c36684e5b02459ce325cedd0e3ba2ebc5ea7086
salman <salmancrc9@gmail.com>
2025-11-19 00:08:17 +0500

    blog page added for blog main route

---
+++ src/app/blog/page.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+
+const posts = [
+  {
+    id: "1",
+    title: "post 1",
+    body: "post 1 body",
+  },
+  {
+    id: "2",
+    title: "post 2",
+    body: "post 2 body",
+  },
+];
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1>Blog posts</h1>
+      {posts.map((post) => (
+        <Link key={post.id} href={`/blog/${post.id}`}>
+          <div className='mb-4'>
+            <h2>{post.title}</h2>
+            <div>{post.body}</div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
