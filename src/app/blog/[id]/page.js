cc67bce97272b74f43d7144cc790414f9bdd13f8
salman <salmancrc9@gmail.com>
2025-11-19 00:08:56 +0500

    refactor post component and removed blog page changes

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -13,26 +13,8 @@ const posts = [
   },
 ];
 
-const BlogPostsPage = ({ params }) => {
-  // If the route is /blog (id is undefined or missing), show list, else show single post
-  const { id } = params || {};
-
-  if (!id) {
-    // Show all posts (for /blog route)
-    return (
-      <div>
-        <h1>Blog posts</h1>
-        {posts.map((post) => (
-          <div key={post.id}>
-            <div>{post.title}</div>
-            <div>{post.body}</div>
-          </div>
-        ))}
-      </div>
-    );
-  }
-
-  // When id is present, show single post (for /blog/[id])
+const BlogPostPage = async ({ params }) => {
+  const { id } = await params;
   const post = posts.find((p) => p.id === id);
 
   if (!post) {
@@ -47,4 +29,4 @@ const BlogPostsPage = ({ params }) => {
   );
 };
 
-export default BlogPostsPage;
+export default BlogPostPage;
