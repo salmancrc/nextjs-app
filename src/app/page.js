d165496b4ece079c1691b11e6c81ab4d1a028a05
salman <salmancrc9@gmail.com>
2025-11-22 21:37:21 +0500

    added nav for home

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-      </main>
+    <div>
+      <nav className="flex gap-4">
+        <Link href="/">Logo</Link>
+        <Link href="/">Home</Link>
+        <Link href="/blog">Blog</Link>
+      </nav>
     </div>
   );
 }
