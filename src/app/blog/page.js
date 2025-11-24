73624cae4d6024057d52fc0ba8fb7a2600745a3b
salman <salmancrc9@gmail.com>
2025-11-24 11:23:05 +0500

    posts moved to client component

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -22,18 +22,6 @@ export default async function BlogPage() {
   return (
     <div>
       <h1>Blog posts</h1>
-      <div className='mt-4'>
-        {posts.map((post) => (
-          <div key={post.id}  className='border mb-4'>
-            <Link href={`/blog/${post.id}`}>
-              <div className='p-4'>
-                <h2 className='text-indigo-500'>{post.title}</h2>
-                <div>{post.body}</div>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
     </div>
   );
 }
