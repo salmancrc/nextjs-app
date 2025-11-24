fe7baf0aac57840df24530fb94e356f89cd8b1cd
salman <salmancrc9@gmail.com>
2025-11-24 14:28:00 +0500

    Refactor BlogPage to use Suspense for loading posts component

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import Link from 'next/link';
-import RootLayout from '../layout';
+import React, { Suspense } from 'react';
+import Posts from './components/posts';
 
 const posts = [
   {
@@ -16,12 +15,15 @@ const posts = [
 ];
 
 export default async function BlogPage() {
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
-  const posts = await data.json()
+  // const data = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
+  // const posts = await data.json()
 
   return (
     <div>
       <h1>Blog posts</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Posts posts={posts} />
+      </Suspense>
     </div>
   );
 }
